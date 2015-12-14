fe12da7f9e7523e226f07cc9e354468eaa1c046b
Paul Boiseau <paulboiseau@gmail.com>
2015-12-14 19:18:37 +0100

    add login and register view controller

---
+++ Clock/Controller/RegisterViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RegisterViewController.swift
+//  clock
+//
+//  Created by Paul on 14/12/2015.
+//  Copyright © 2015 paulboiseau. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func registerUser() {
+        
+    }
+    
+    @IBAction func callRegisterUser(sender: UIButton) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
